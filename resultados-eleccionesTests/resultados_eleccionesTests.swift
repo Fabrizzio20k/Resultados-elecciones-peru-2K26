7cd50e531d32acfdc68f0c11f5d09f82e201ed26
Fabrizzio20k <fabrizzio785@gmail.com>
2026-06-14 01:38:31 -0500

    Initial Commit

---
+++ resultados-eleccionesTests/resultados_eleccionesTests.swift
@@ -0,0 +1,19 @@
+//
+//  resultados_eleccionesTests.swift
+//  resultados-eleccionesTests
+//
+//  Created by Fabrizzio on 14/06/26.
+//
+
+import Testing
+@testable import resultados_elecciones
+
+struct resultados_eleccionesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
